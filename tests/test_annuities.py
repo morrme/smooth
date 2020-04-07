56ce57ca376d055a501e5610356dc6eb925a7cdb
jtm91 <jtm91@mailbox.tu-berlin.de>
2020-04-07 12:13:53 +0200

    Update annuity tests

--- tests/test_annuities.py
+++ tests/test_annuities.py
@@ -1,5 +1,5 @@
 import pytest
-from smooth.framework.functions.update_annuities import update_annuities #, calc_annuity
+from smooth.framework.functions.update_annuities import update_annuities, calc_annuity, calc_annual_emissions
 from smooth.components.component import Component
 from smooth.framework.simulation_parameters import SimulationParameters as sp
 
@@ -27,24 +27,34 @@ class TestAnnuities:
         self.component.life_time = 20
         self.component.capex = capex
         self.component.sim_params = sp(sim_params)
+        self.target = {'cost':10}
 
-        # def test_calc_annuities(self):
-    #     assert (round(calc_annuity(component, None)) == 0)
-    #     assert (round(calc_annuity(component, {})) == 0)
+    def test_calc_annuities(self):
+        assert (round(calc_annuity(self.component, None)) == 0)
+        assert (round(calc_annuity(self.component, {})) == 0)
 
-    def test_update_annuities1(self):
+    def test_update_annuities_error(self):
         self.component.life_time = None
         with pytest.raises(ValueError):
             self.component.check_validity()
+        self.component.life_time = 0
+        with pytest.raises(ValueError):
+            self.component.check_validity()
 
-    def test_update_annuities2(self):
-        update_annuities(self.component)
-        assert self.component.results is not None
-        # TODO change test values
-        # capex_annuity = 1
-        # opex = 2
-        # variable_cost_annuity = 3
-        # assert self.component.results['annuity_capex'] == capex_annuity
-        # assert self.component.results['annuity_opex'] == opex
-        # assert self.component.results['annuity_variable_costs'] == variable_cost_annuity
-        # assert self.component.results['annuity_total'] == capex_annuity + opex + variable_cost_annuity
+    # def test_update_annuities_results(self):
+    #     with pytest.raises(Exception):
+    #         update_annuities(self.component)
+    #     # assert self.component.results is not None
+    #     # TODO change test values
+    #     # capex_annuity = 1
+    #     # opex = 2
+    #     # variable_cost_annuity = 3
+    #     # assert self.component.results['annuity_capex'] == capex_annuity
+    #     # assert self.component.results['annuity_opex'] == opex
+    #     # assert self.component.results['annuity_variable_costs'] == variable_cost_annuity
+    #     # assert self.component.results['annuity_total'] == capex_annuity + opex + variable_cost_annuity
+
+    def test_calc_annuities_emissions(self):
+        assert calc_annual_emissions(self.component, None) == 0
+        assert calc_annual_emissions(self.component, 0) == 0
+        assert calc_annual_emissions(self.component,self.target) == self.target['cost'] / self.component.life_time
